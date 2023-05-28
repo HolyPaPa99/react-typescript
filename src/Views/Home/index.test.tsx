a8790d9114d443b75969d0d251c6d2109a4d51d8
李烨烽 <liyefeng@admindeMacBook-Air.local>
2023-05-28 19:12:51 +0800

    jest

---
+++ src/Views/Home/index.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer'
+import Home from '.'
+
+it('Home', () => {
+    const component = renderer.create(
+        <BrowserRouter>
+            <Home />
+        </BrowserRouter>
+        ,);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
--- src/Views/Home/index.tsx
+++ src/Views/Home/index.tsx
@@ -3,30 +3,27 @@ import { Outlet } from 'react-router';
 import { Link } from 'react-router-dom';
 import styles from './index.module.scss';
 
-class Home extends React.Component {
-
-    render() {
-        return (
-            <div style={{ display: 'flex' }}>
-                <div style={{ marginRight: '20px' }}>
-                    <h1>Hello, World!</h1>
-                    <hr />
-                    <ul className={styles.ul}>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="page1/1/2">Page1</Link>
-                        </li>
-                        <li>
-                            <Link to="page2">Page2</Link>
-                        </li>
-                    </ul>
-                </div>
-                <Outlet />
+function Home() {
+    return (
+        <div style={{ display: 'flex' }}>
+            <div style={{ marginRight: '20px' }}>
+                <h1>Hello, World!</h1>
+                <hr />
+                <ul className={styles.ul}>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="page1/1/2">Page1</Link>
+                    </li>
+                    <li>
+                        <Link to="page2">Page2</Link>
+                    </li>
+                </ul>
             </div>
-        );
-    }
+            <Outlet />
+        </div>
+    );
 }
 
 export default Home;
